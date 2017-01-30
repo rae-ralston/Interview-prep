c52ec898556dfe0bca87393be1228c663b765200
Rachel Ralston <rachelralston@gmail.com>
2017-01-30 13:35:12 -0800

    updated practice exercies

--- hackerRank_practice/warmUp/08_circularArrayRotation.js
+++ hackerRank_practice/warmUp/08_circularArrayRotation.js
@@ -17,24 +17,38 @@ Sample Output
 3
 1
 */
-array = [1,2,3]
+let arr = [1,2,3]
 
-let circularArrayRotation = (array, rotations, query) => {
+let circularArrayRotation = (arr, rotations) => {
   for(let i = 0; i < rotations; i++) {
-    let temp = array[array.length - 1]
-    for(let j = array.length - 1; j <= 0; j--){
-      if(j === 0){
-        array[j] = temp
-        console.log('if j = 0', array[j])
-      } else {
-        array[j] = array[j-1]
-        console.log('if j > 0', array[j])
-      }
-    }
+    let move = arr.pop()
+    console.log('move', move)
+    arr.splice(0,0,move)
+    console.log('the arr', arr)
   }
-  // for(let k = 0; k <= query - 1; k++){
-  //   console.log(array[k])
-  // }
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+  }
 }
 
-circularArrayRotation( array, 2, 3)
+circularArrayRotation( arr, 2 )
+
+// let circularArrayRotation = (array, rotations, query) => {
+//   for(let i = 0; i < rotations; i++) {
+//     let temp = array[array.length - 1]
+//     for(let j = array.length - 1; j <= 0; j--){
+//       if(j === 0){
+//         array[j] = temp
+//         console.log('if j = 0', array[j])
+//       } else {
+//         array[j] = array[j-1]
+//         console.log('if j > 0', array[j])
+//       }
+//     }
+//   }
+//   // for(let k = 0; k <= query - 1; k++){
+//   //   console.log(array[k])
+//   // }
+// }
+
+// circularArrayRotation( array, 2, 3)
