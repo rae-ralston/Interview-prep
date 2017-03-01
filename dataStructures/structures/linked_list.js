d2b7385a84528e4e7d329c33272624cd053fefbc
Rachel Ralston <rachelralston@gmail.com>
2017-02-28 16:48:41 -0800

    setting up linked list

---
+++ dataStructures/structures/linked_list.js
@@ -0,0 +1,70 @@
+export default class LinkedList {
+  constructor() {
+
+  }
+
+// linkedList.getHeadNode()           // Returns the first node in the list
+  getHeadNode() {
+
+  }
+
+// linkedList.getTailNode()           // Returns the last node in the list
+  getTailNode() {
+
+  }
+
+// linkedList.contains("bananas")     // Determines whether or not the list contains the provided data
+  contains(element) {
+
+  }
+
+// linkedList.find("bananas")         // Returns the first node containing the provided data, or -1 if not found
+  find(element) {
+
+  }
+
+// linkedList.insert("bananas")       // Inserts a node (with the provided data) to the tail of the list
+  insert(element) {
+
+  }
+
+// linkedList.insertFirst("bananas")  // Inserts a node (with the provided data) to the head of the list
+  insertFirst(element) {
+
+  }
+
+// linkedList.insertBefore("bananas", "apples") // Inserts a node (with data "apples") before the first node containing "bananas"
+  insertBefore(element, data) {
+
+  }
+
+// linkedList.insertAfter("apples", "bananas")  // Inserts a node (with data "bananas") after the first node containing "apples"
+  insertAfter(element, data) {
+
+  }
+
+// linkedList.remove()                // Removes the tail node from the list
+  remove() {
+
+  }
+
+// linkedList.removeFirst()           // Removes the head node from the list
+  removeFirst() {
+
+  }
+
+// linkedList.isEmpty()               // Determines if the list is empty or not
+  isEmpty() {
+
+  }
+
+// linkedList.size()                  // Returns the size of the list (number of nodes)
+  size() {
+
+  }
+
+// linkedList.clear() 
+  clear() {
+
+  }
+}
