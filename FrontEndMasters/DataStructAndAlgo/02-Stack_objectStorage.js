722077bae007b33204d7379bafa02c549621f7a5
Rachel Ralston <rachelralston@gmail.com>
2017-04-04 13:33:29 -0700

    working on stack with object storage

---
+++ FrontEndMasters/DataStructAndAlgo/02-Stack_objectStorage.js
@@ -0,0 +1,24 @@
+class Stack {
+  constructor() {
+    this.storage = {}
+    this.length = 0
+  }
+
+  push(value) {
+  }
+
+  pop() {
+  }
+
+  peak() {
+  }
+
+  size() {
+  }
+
+  contains(value) {
+  }
+
+  until(value){
+  }
+}
