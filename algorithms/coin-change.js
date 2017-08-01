286557db097a16e7cda6230f3905c731d282651d
Rachel Ralston <rachelralston@gmail.com>
2017-08-01 09:16:53 -0700

    working through coin change algo from hackerrank

---
+++ algorithms/coin-change.js
@@ -0,0 +1,11 @@
+function getWays(n, c){
+    let sortedCoins = c.sort((a, b) => a - b).reverse()
+    console.log(n, sortedCoins)
+    let coinList = []
+    // is arr[0] *2 > n?
+
+}
+
+
+getWays(10, [ 2, 5, 3, 6 ])
+getWays(4, [ 1, 2, 3 ])
